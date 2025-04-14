2c17482db5091fd11d08d60f52e4237ca2eca094
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-14 14:36:09 +0300

    Gun05 - _04_Navigating

--- src/Gun05/_03_GetCssValueYeni.java
+++ src/Gun05/_03_GetCssValueYeni.java
@@ -4,7 +4,7 @@ import Utlity.BaseDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class _04_GetCssValueYeni extends BaseDriver {
+public class _03_GetCssValueYeni extends BaseDriver {
     public static void main(String[] args) {
         driver.get("https://www.snapdeal.com/");
 
