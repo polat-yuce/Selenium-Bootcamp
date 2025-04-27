c02fdb36d5f0ad746de624b9c3d234d9b92ba272
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-27 13:11:17 +0300

    Gun14 robotGiris update

--- src/Gun14/_01_RobotGiris.java
+++ src/Gun14/_01_RobotGiris.java
@@ -1,8 +1,17 @@
 package Gun14;
 
+/*
+   Interview Sorusu
+
+   Windows pencerelerini nasıl kontrol edersin.
+   ROBOT Class la kontrol ederim.
+ */
+
+
 import Utlity.BaseDriver;
 import Utlity.MyFunc;
 import org.junit.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -12,27 +21,29 @@ import java.util.Set;
 public class _01_RobotGiris extends BaseDriver {
 
     @Test
-    public void Test() throws AWTException {
+    public void Test1() throws AWTException {
         driver.get("https://www.selenium.dev/");
         MyFunc.Bekle(2);
 
-        Robot robot=new Robot();
+        Robot robot = new Robot();
 
-        // Yeni pencerede açma işlemi ctrl+t
-        //robot.keyPress(KeyEvent.VK_CONTROL); // ctrl bas, şu an artık basılı durumda
-        robot.keyPress(KeyEvent.VK_META); // ctrl bas, şu an artık basılı durumda
+        driver.switchTo().window(driver.getWindowHandle()); // Mac için
+        robot.keyPress(KeyEvent.VK_META); // Mac lerde CTRL
+
+        //Yeni pencere açma işlemi  ctlr+T
+        // robot.keyPress(KeyEvent.VK_CONTROL);// ctrl bas, şu an artık basılı durumda
         robot.keyPress(KeyEvent.VK_T); // ctrl basılı idi, T harfine bastım
         robot.keyRelease(KeyEvent.VK_T); // T tuşunu bırak
-        //robot.keyRelease(KeyEvent.VK_CONTROL); //CTRL tuşunu bırak
-        robot.keyRelease(KeyEvent.VK_META); //CTRL tuşunu bırak
+        // robot.keyRelease(KeyEvent.VK_CONTROL);  // CTRL tuşunu bırak
+        robot.keyRelease(KeyEvent.VK_META);  // CTRL tuşunu bırak
         MyFunc.Bekle(2);
 
         System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl()); // şu an hala 1.sayfadayım
 
-        Set<String> idLer=driver.getWindowHandles();
-        Iterator gosterge= idLer.iterator();
-        String birinciPencereID= gosterge.next().toString();
-        String ikinciPencereID= gosterge.next().toString();
+        Set<String> idLer = driver.getWindowHandles();
+        Iterator gosterge = idLer.iterator();
+        String birinciPencereID = gosterge.next().toString();
+        String ikinciPencereID = gosterge.next().toString();
 
         driver.switchTo().window(ikinciPencereID); //2.Pencereye yani taba geçctim.
         MyFunc.Bekle(2);
