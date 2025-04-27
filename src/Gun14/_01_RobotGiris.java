be9488b50ee63eb5893c11ecc9e2a70a47331cdd
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-27 13:08:18 +0300

    Gun14 robotGiris

---
+++ src/Gun14/_01_RobotGiris.java
@@ -0,0 +1,44 @@
+package Gun14;
+
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.Iterator;
+import java.util.Set;
+
+public class _01_RobotGiris extends BaseDriver {
+
+    @Test
+    public void Test() throws AWTException {
+        driver.get("https://www.selenium.dev/");
+        MyFunc.Bekle(2);
+
+        Robot robot=new Robot();
+
+        // Yeni pencerede açma işlemi ctrl+t
+        //robot.keyPress(KeyEvent.VK_CONTROL); // ctrl bas, şu an artık basılı durumda
+        robot.keyPress(KeyEvent.VK_META); // ctrl bas, şu an artık basılı durumda
+        robot.keyPress(KeyEvent.VK_T); // ctrl basılı idi, T harfine bastım
+        robot.keyRelease(KeyEvent.VK_T); // T tuşunu bırak
+        //robot.keyRelease(KeyEvent.VK_CONTROL); //CTRL tuşunu bırak
+        robot.keyRelease(KeyEvent.VK_META); //CTRL tuşunu bırak
+        MyFunc.Bekle(2);
+
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl()); // şu an hala 1.sayfadayım
+
+        Set<String> idLer=driver.getWindowHandles();
+        Iterator gosterge= idLer.iterator();
+        String birinciPencereID= gosterge.next().toString();
+        String ikinciPencereID= gosterge.next().toString();
+
+        driver.switchTo().window(ikinciPencereID); //2.Pencereye yani taba geçctim.
+        MyFunc.Bekle(2);
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+        driver.get("https://www.facebook.com/");
+
+        BekleKapat();
+    }
+}
